7837aa1182ca1ac3462de4fcc6fc3007ac1b99c7
Beerendan <brendanrbowen@gmail.com>
2023-07-06 14:38:43 -0400

    added findAll in products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,7 +6,31 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes:[
+      'id', 'product_name','price','stock','category_id'
+    ],
+    include:[{
+      model: Category,
+      attributes:[
+        'id','category_name',
+      ]
+    },
+    {
+      model: Tag,
+      attributes:[
+        'id','tag_name'
+      ]
+    }
+    ]
+  })
+  //return results to be displayed
+  .then(allProductData=>res.json(allProductData))
+
+  .catch(err=>{
+    console.log(err);
+    res.status(500).json(err)
+  });
 });
 
 // get one product
