2524aac68cef3247c51cbedd0f1470f3ab9a4594
Beerendan <brendanrbowen@gmail.com>
2023-07-06 15:18:37 -0400

    added findOne to tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { on } = require('nodemon');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
@@ -24,6 +25,7 @@ router.get('/', (req, res) => {
       }
     ]
   })
+  
  .then(allTagData=>res.json(allTagData))
  .catch(err=>{
   console.log(err);
@@ -33,7 +35,38 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where:{
+      id: req.params.id
+    },
+    attributes:[
+      'id',
+      'tag_name'
+    ],
+ //include its associated Product data
+    include:[{
+      attributes:[
+        'id',
+        'product_name',
+        'price',
+        'stock',
+        'category_id'
+      ]
+    }]
+})
+
+ .then(oneTagData=>{
+  if(!oneTagData){
+    res.status(404).json(err)
+    return;
+  };
+  res.json(oneTagData);
+ })
+
+ .catch(err=>{
+  console.log(err);
+  res.status(500).json(err);
+ });
 });
 
 router.post('/', (req, res) => {
